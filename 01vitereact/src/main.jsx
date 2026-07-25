ac30b0b97880cd4f57a058cd5674863410a06aee
mayanknaruka <mayanknaruka47@gmail.com>
2026-07-25 23:46:50 +0530

    custom react

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <div>
+       <h1>custom app</h1>
+    </div>
+  )
+}
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <MyApp />
 )
