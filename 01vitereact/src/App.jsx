d79679bcd8c5f23c602aa1498d8fa0eb278f0e4d
mayanknaruka <mayanknaruka47@gmail.com>
2026-07-25 23:57:03 +0530

    done for today

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,17 @@
+
+
 import Chai from "./chai"
 
+
 function App() {
+  const username = "chai aur code"
 
   return (
-     <Chai/>
-     
+    <>
+    <Chai/>
+    <h1>chai aur react {username}</h1>
+    <p>test para</p>
+   </>
   )
 }
 
