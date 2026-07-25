12c5d363abb722beb58cc7bac2a6e18b11d9d9e3
mayanknaruka <mayanknaruka47@gmail.com>
2026-07-25 13:42:22 +0530

    understand the react flow

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,8 +1,10 @@
 
-
+import Chai from "./chai";
 function App() {
   return (
-    <h1>hello world</h1>
+    <>
+      <Chai />
+    </>
   );
 }
 
--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,10 @@
-
+import Chai from "./chai"
 
 function App() {
 
   return (
-   <h1>Chai aur react</h1>   
+     <Chai/>
+     
   )
 }
 
