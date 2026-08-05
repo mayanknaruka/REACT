f932cf09a5ca4cb3a5fbe02a0cc070e0a702a53b
mayanknaruka <mayanknaruka47@gmail.com>
2026-08-05 21:35:44 +0530

    done

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -1,22 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-
-function App() {
-  const [color, setColor] = useState("olive")
-    
-  return (
-    <div className='w-full h-screen duration-200'
-    style={{backgroundColor: color}}
-
-    ></div>
-   
-  )
-}
-
-export default App
 import { useState } from "react"
 
 
